d23c65436016445f6fa2a6898d7a78252536fe04
Carl Kadie <carlk@microsoft.com>
2017-01-10 15:30:17 -0800

    Update version

--- fastlmm/association/tests/test_snp_set.py
+++ fastlmm/association/tests/test_snp_set.py
@@ -159,8 +159,8 @@ if __name__ == '__main__':
     from fastlmm.association.tests.test_snp_set import TestSnpSet
     suites = unittest.TestSuite([getTestSuite()])
 
-    if False: #Standard test run
-        r = unittest.TextTestRunner(failfast=True) #!!!cmk set to False
+    if True: #Standard test run
+        r = unittest.TextTestRunner(failfast=False)
         r.run(suites)
     else: #Cluster test run
         from fastlmm.util.distributabletest import DistributableTest
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.command.clean import clean as Clean
 import numpy
 
 # Version number
-version = '0.2.26'
+version = '0.2.31'
 
 
 def readme():
